ac7850655b4d5415f543cb4f3ec050be6175ca38
princy <princy@braincuber.com>
2022-07-05 15:22:35 +0530

    added lambda functions

---
+++ src/get.js
@@ -0,0 +1,27 @@
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
+module.exports.handler = async (event) => {
+  console.log(event);
+  const response = {
+    isBase64Encoded: false,
+    statusCode: 200,
+  };
+  try {
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: decodedFile(event.pathParameters.fileKey),
+    };
+    const getResult = await s3.getObject(params).promise();
+    response.body = JSON.stringify({ message: "Successfully retrived file" });
+  } catch (e) {
+    console.error(e);
+    response.body = JSON.stringify({
+      message: "Failed to retrieve file",
+      errorMessage: e,
+    });
+    response.statusCode = 500;
+  }
+  return response;
+};
